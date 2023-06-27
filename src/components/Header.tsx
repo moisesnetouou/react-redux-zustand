2299a2fab56f85e40fd5d00262fefead3c517991
Moisés <moisesnetored@gmail.com>
2023-06-27 15:41:35 -0400

    refac: separating components

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="flex flex-col gap-1">
+      <h1 className="text-wxl font-bold">Fundamentos do Redix</h1>
+      <span className="text-sm text-zinc-400">Módulo "Desventando Redux"</span>
+    </div>
+  )
+}
