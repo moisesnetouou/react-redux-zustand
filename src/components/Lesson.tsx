721178a8c8c67515ffb0756c7a199700010ddd24
Moisés <moisesnetored@gmail.com>
2023-06-28 08:38:04 -0400

    feat: select current lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -3,11 +3,15 @@ import { Video } from 'lucide-react'
 interface LessonProps {
   title: string
   duration: string
+  onPlay: () => void
 }
 
-export function Lesson({ duration, title }: LessonProps) {
+export function Lesson({ duration, title, onPlay }: LessonProps) {
   return (
-    <button className="flex items-center gap-3 text-sm text-zinc-400">
+    <button
+      className="flex items-center gap-3 text-sm text-zinc-400"
+      onClick={onPlay}
+    >
       <Video className="w-4 h-4 text-zinc-500" />
 
       <span>{title}</span>
