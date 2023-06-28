017a88eee23aabe51957cf7e76fa419eb2e81390
Moisés <moisesnetored@gmail.com>
2023-06-28 11:06:21 -0400

    feat: highlighting current class

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,22 @@
+import { useAppSelector } from '../store'
+
 export function Header() {
+  const { currentLesson, currentModule } = useAppSelector((state) => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    const currentModule = state.player.course.modules[currentModuleIndex]
+
+    const currentLesson = currentModule.lessons[currentLessonIndex]
+
+    return { currentLesson, currentModule }
+  })
+
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-wxl font-bold">Fundamentos do Redix</h1>
-      <span className="text-sm text-zinc-400">Módulo "Desventando Redux"</span>
+      <h1 className="text-wxl font-bold">{currentLesson.title}</h1>
+      <span className="text-sm text-zinc-400">
+        Módulo "{currentModule.title}"
+      </span>
     </div>
   )
 }
